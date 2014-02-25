07e3e0d3bb0e18c2be940469da6743ab294f8679
AllisterSmith <allistersmithster@gmail.com>
2014-02-25 08:22:21 -0500

    Testing ino clean function

--- level/.build/mega2560/arduino/WInterrupts.d
+++
@@ -1,6 +0,0 @@
-.build/mega2560/arduino/WInterrupts.d .build/mega2560/arduino/WInterrupts.o:  \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WInterrupts.c \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/wiring_private.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/mega/pins_arduino.h
