5556197184267a2b9eb0343836d98a6b0533cfca
AllisterSmith <allistersmithster@gmail.com>
2014-02-27 23:36:53 -0500

    Updated missing lib files

--- main/.build/uno/SD/SD.d
+++ main/.build/uno/SD/SD.d
@@ -1,5 +1,4 @@
-.build/uno/src/HydroController_beta_from_Billies_code.d .build/uno/src/HydroController_beta_from_Billies_code.o:  \
- .build/uno/src/HydroController_beta_from_Billies_code.cpp \
+.build/uno/SD/SD.d .build/uno/SD/SD.o: lib/SD/SD.cpp lib/SD/SD.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WCharacter.h \
@@ -10,10 +9,8 @@
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Printable.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/standard/pins_arduino.h \
-  lib/SD/SD.h lib/SD/utility/SdFat.h lib/SD/utility/Sd2Card.h \
+  lib/SD/utility/SdFat.h lib/SD/utility/Sd2Card.h \
   lib/SD/utility/Sd2PinMap.h lib/SD/utility/SdInfo.h \
   lib/SD/utility/FatStructs.h \
   /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Print.h \
-  lib/SD/utility/SdFatUtil.h lib/Wire/Wire.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Stream.h \
-  lib/RTClib/RTClib.h lib/EEPROMEx/EEPROMex.h lib/OneWire/OneWire.h
+  lib/SD/utility/SdFatUtil.h
