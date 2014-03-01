eb7d46157ca7e6bc19cd7c2e3186090d6450ee77
AllisterSmith <allistersmithster@gmail.com>
2014-03-01 00:39:34 -0500

    cleanup

--- main/.build/mega2560/Wire/utility/twi.d
+++
@@ -1,5 +0,0 @@
-.build/mega2560/Wire/utility/twi.d .build/mega2560/Wire/utility/twi.o: lib/Wire/utility/twi.c \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/mega/pins_arduino.h \
-  lib/Wire/utility/twi.h
--- main/.build/mega2560/arduino/WMath.d
+++
@@ -1,2 +0,0 @@
-.build/mega2560/arduino/WMath.d .build/mega2560/arduino/WMath.o:  \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WMath.cpp
