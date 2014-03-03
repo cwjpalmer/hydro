60f8ac20f7e5a2b7189a138667b8cc03230af9df
cwjpalmer <cwjpalmer@gmail.com>
2014-03-03 13:38:20 +0100

    added sketch to test non-dallas alternative for waterproof temperature sensor

---
+++ waterproof-temperature-sensor-dallas/dallas/.build/uno/src/waterproof_temperature_sensor_via_dallas.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <OneWire.h>                       //OneWire library, for liquid temperature sensor
+#include <DallasTemperature.h>             //Library for Dallas Temperature that may or may not be required for liquid temperature sensor 
+void setup(void);
+void loop ();
+#line 1 "src/waterproof_temperature_sensor_via_dallas.ino"
+// this is not an arduino sketch, but rather code that can be included to add a function to a sketch
+// the function liquidTemperatureRead determines the temperature using a one-wire temperature sensor
+
+//#include <OneWire.h>                       //OneWire library, for liquid temperature sensor
+//#include <DallasTemperature.h>             //Library for Dallas Temperature that may or may not be required for liquid temperature sensor 
+
+
+#define ONE_WIRE_BUS 28      // pin for temperature sensor
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+// put this somewhere in: void setup(){}
+float temp;
+
+
+void setup(void)
+{
+pinMode (ONE_WIRE_BUS,OUTPUT);
+// start serial port
+Serial.begin(9600);
+Serial.println("Dallas Temperature IC Control Library Demo");
+ 
+// Start up the library
+sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+}
+
+
+
+//add this function and all it when you need a temperature reading
+
+void loop () {
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  delay(1000);
+  Serial.println("DONE");
+  Serial.print("Liquid temperature is: ");
+  temp = sensors.getTempCByIndex(0);
+  Serial.println(temp);
+  
+}
+
---
+++ waterproof-temperature-sensor-dallas/dallas/.build/uno/src/waterproof_temperature_sensor_via_dallas.d
@@ -0,0 +1,13 @@
+.build/uno/src/waterproof_temperature_sensor_via_dallas.d .build/uno/src/waterproof_temperature_sensor_via_dallas.o:  \
+ .build/uno/src/waterproof_temperature_sensor_via_dallas.cpp \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WCharacter.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WString.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Stream.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Print.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Printable.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/standard/pins_arduino.h \
+  lib/OneWire/OneWire.h lib/DallasTemperature/DallasTemperature.h
