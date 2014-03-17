900b96569254fe6a3991ba746516f5dca3aad83f
cwjpalmer <cwjpalmer@gmail.com>
2014-03-17 22:43:40 +0100

    first two portions of commands are now case in-sensitive

--- semantic-serial-sandbox/src/sketch.ino
+++ semantic-serial-sandbox/src/sketch.ino
@@ -5,9 +5,7 @@ char* c1;
 char* c2;
 char* c3;
 
-float ph=4.0;
-int tankLowSetPoint = 20;
-int tankHighSetPoint = 80;
+float ph=4.0; // THIS IS IN THE MAIN SKETCH !!! chance ph throughout to match name of ph set point in main sketch
 
 void setup() {
 	Serial.begin(9600);
@@ -39,15 +37,25 @@ void loop () {
 		Serial.print("c3 is: ");
 		Serial.println(c3);
 
+		// make the character array lowercase
+		String c1string = String(c1); 					// make a String from the character array c1
+		c1string.toLowerCase();							// make the String lower case
+		c1string.toCharArray(c1,c1string.length()+1);	// send the lower case String characters back to the character array c1
+	
+		String c2string = String(c2); 					// make a String from the character array c1
+		c2string.toLowerCase();							// make the String lower case
+		c2string.toCharArray(c2,c2string.length()+1);	// send the lower case String characters back to the character array c1
+
+
 		if (strcmp(c1,"ph") == 0) {
-			Serial.println("command ph recognized");
+			//Serial.println("command ph recognized");
 			
 			if (strcmp(c2,"set") == 0) {
-				Serial.println("command ph set recognized");
+				// Serial.println("command ph set recognized");
 				ph = atof(c3);
 			}
-			else if (strcmp(c2,"increase") == 0) {
-				Serial.println("command ph increase recognized");
+			else if (strcmp(c2,"increase") == 0 || strcmp(c2,"inc") == 0) {
+				// Serial.println("command ph increase recognized");
 				if (c3) {
 					//Serial.println("c3 is present");
 					ph = ph + atof(c3);
@@ -56,18 +64,25 @@ void loop () {
 					ph = ph + 0.1;
 				}
 			}
-			else if (strcmp(c2,"decrease") == 0) {
-				Serial.println("command ph decrease recognized");
+			else if (strcmp(c2,"decrease") == 0 || strcmp(c2,"dec") == 0) {
+				// Serial.println("command ph decrease recognized");
+				if (c3) {
+					//Serial.println("c3 is present");
+					ph = ph - atof(c3);
+				}
+				else {
+					ph = ph - 0.1;
+				}
 			}
 			else {
-				Serial.println("pH comand not recognized. try again.");
+				// Serial.println("pH comand not recognized. try again.");
 			}
 		}
 		// elseif() {
 
 		// }
 		else {
-			Serial.println("comand not recognized. try again.");
+			Serial.println("comand received, but not recognized. check formatting and try again.");
 		}
 	
 	}
