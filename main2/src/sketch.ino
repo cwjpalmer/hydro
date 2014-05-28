79f801de9796644c795f951818c5daec5fe809cd
cwjpalmer <cwjpalmer@gmail.com>
2014-05-28 19:41:41 +0200

    small changes, semantic serial still not working; got semantic serial working in separate script, will add directly to main; DELETE THIS BRANCH

--- main2/src/sketch.ino
+++ main2/src/sketch.ino
@@ -1,4 +1,4 @@
-  /* 
+  /*
   THIS VERSION IS MODIFIED FROM THE ORIGINAL VERSION
   ALL CODE PERTAINING TO THE LCD SCREEN IS BEING COMMENTED OUT
   ALL OTHER CODE IS BEING ANNOTATED TO FACILITATE A STRONGER UNDERSTANDING OF THE FUNCTIONALITY OF THE CODE
@@ -10,23 +10,23 @@
     ****Made by Tom De Bie for anyone who can find a use for it ;)****
     ******************************************************************
     ******************************************************************
-  
+
     */
 
     #include <SD.h>                            //SD card library
     #include <Wire.h>                          //One Wire library
     #include "RTClib.h"                        //Real Time Clock library
-    #include <EEPROMex.h>                      //Extended Eeprom library
+  //#include <EEPROMex.h>                      //Extended Eeprom library
     #include <OneWire.h>                       //OneWire library, for liquid temperature sensor
-    #include "DHT.h"                           //DHT library for DHT22 sensor 
-    #include "SPI.h"
-  //#include <DallasTemperature.h>             //Library for Dallas Temperature that may or may not be required for liquid temperature sensor 
+    #include "DHT.h"                           //DHT library for DHT22 sensor
+  //#include "SPI.h"
+  //#include <DallasTemperature.h>             //Library for Dallas Temperature that may or may not be required for liquid temperature sensor
 
 
 
     #define DHTPIN 34                          //pin for DHT22                      // [  ] 1 digital input, 10KOhm resistor, 5V
     #define redLEDpin 2                        //LEDs on SD card
-    #define greenLEDpin 3                      //LEDs on SD Card 
+    #define greenLEDpin 3                      //LEDs on SD Card
     #define DHTTYPE DHT22                      // DHT 22  (AM2302)
 
     #define ECHO_TO_SERIAL   1                 // echo data to serial port
@@ -38,8 +38,8 @@
     OneWire ds(DS18S20_Pin);  // on digital pin 2
 
     // liquid level
-    #define liqLevelRefResistor 2250           // [] it's 2250 ohms +/- 10%, so we should check it with a multimeter and put the correct value here    
-    #define liqLevelSensorPin 48 
+    #define liqLevelRefResistor 2250           // [] it's 2250 ohms +/- 10%, so we should check it with a multimeter and put the correct value here
+    #define liqLevelSensorPin 48
 
     // LED Pins to represent control systems
     #define LED_SOLENOID_PIN 38
@@ -56,7 +56,7 @@
     int ventilatorPin = 47;                    //pin for Fan (relay)                // [  ] digital Pin
     int lightSensor = A2;                      //pin for Photoresistor              // [  ]   1 analog input, 10kOhm resistor, 5V
     int solenoidPin = 49;                      //digital pin
-    float h;                                   //humidity 
+    float h;                                   //humidity
     float t;                                   //temperature
   //int liquidTemperaturePin = 2;              //digital pin   LIQTfindMeTag
     char filename[] = "LOGGER00.CSV";          //filename for CSV file
@@ -65,7 +65,7 @@
     int tankHighSetPoint = 80;                          // variable to store upper limit of tank level   // as a %
 
 
-    File logfile;                               //indicate CSV file exists 
+    File logfile;                               //indicate CSV file exists
     RTC_DS1307 RTC;                            //Define RTC module
 
     //****************************************************************//
@@ -79,11 +79,11 @@
 
     int pmem = 0;                      //check which page you're on
     float Setpoint=4.0;                    //holds value for Setpoint
-    float HysterisMin;                 
+    float HysterisMin;
     float HysterisPlus;
     float SetHysteris;
-    float FanTemp = 15;                   
-    float FanHumid = 40;                 
+    float FanTemp = 15;
+    float FanHumid = 40;
     //float liquidTemperature;           // variable to hold temperature of liquid from waterproof thermometer LIQTfindMeTag
 
     int lightADCReading;
@@ -100,7 +100,7 @@
 
     byte bGlobalErr;    //for passing error code back.
 
-         
+
          // - DISABLE EEPROM WHEN NOT REQUIRED - specified life of 100k write/erase cycles
          void EepromRead() { // memory whose values are kept when board loses power  - specified life of 100k write/erase cycles
           //Setpoint = EEPROM.readFloat(EepromSetpoint);
@@ -119,9 +119,9 @@
         pinMode(LED_FAN_PIN, OUTPUT);
         pinMode (LED_LIQ_PIN, OUTPUT);
         //OneWire ds(liquidTemperaturePin); //LIQTfindMeTag
-         
+
         pmem==0;
-         
+
         delay(300);
         Serial.println("System Booting!");
         Serial.println("__________________________________\n\n");
@@ -133,22 +133,22 @@
         // function to determine the liquid level from a sensor value; sensor value is input
         int getLiqLevel() {
           float reading;
- 
+
           reading = analogRead(liqLevelSensorPin);
-         
-          Serial.print("Analog reading "); 
+
+          Serial.print("Analog reading ");
           Serial.println(reading);
-         
+
           // convert the value to resistance
           reading = (1023 / reading)  - 1;
           reading = liqLevelRefResistor / reading;
-          Serial.print("Sensor resistance "); 
+          Serial.print("Sensor resistance ");
           Serial.println(reading);
           return reading;
         }
 
 
-        void logicLoop() { // 
+        void logicLoop() { //
 
         // Reading temperature or humidity takes about 250 milliseconds!
         // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -161,21 +161,21 @@
         if (isnan(t) || isnan(h)) {
           Serial.println("Failed to read from DHT");
         } else {
-          Serial.print("Humidity: "); 
+          Serial.print("Humidity: ");
           Serial.print(h);
           Serial.print(" % RH\t");
-          Serial.print("Temperature: "); 
+          Serial.print("Temperature: ");
           Serial.print(t);
-          Serial.println(" °C");        
-          }      
-                           
+          Serial.println(" °C");
+          }
+
           float sensorValue = 0;
           sensorValue = analogRead(pHPin);
           pH = (0.0178 * sensorValue - 1.889);
 
           HysterisMin = (Setpoint - SetHysteris);
           HysterisPlus = (Setpoint + SetHysteris);
-           
+
           // - SERIES OF IF STATEMENTS TO CHANGE CONTROL VARIABLES BASED ON SYSTEM STATE -
           if (pH == Setpoint) {
             pmem == 0, // why is this a logical operator?
@@ -187,7 +187,7 @@
             digitalWrite (pHMinPin, LOW);
             digitalWrite (pHPlusPin, LOW);
           }
-         
+
           if (pH < HysterisMin && pmem == 0) {
             pmem == 1,
             digitalWrite (pHPlusPin, HIGH);
@@ -195,26 +195,26 @@
             digitalWrite (pHPlusPin, LOW);       // LED
             digitalWrite (pHMinPin, LOW);
           }
-         
+
           if (pH >= HysterisMin && pH < Setpoint && pmem == 1) {
             digitalWrite (pHPlusPin, HIGH);
             delay(300);                          // LED
-            digitalWrite (pHPlusPin, LOW);       // LED            
+            digitalWrite (pHPlusPin, LOW);       // LED
             digitalWrite (pHMinPin, LOW);
           }
-         
+
           if (pH > HysterisPlus && pmem == 0) {
             pmem ==2,
             digitalWrite (pHMinPin, HIGH);
             delay(300);                          // LED
-            digitalWrite (pHMinPin, LOW);        // LED            
+            digitalWrite (pHMinPin, LOW);        // LED
             digitalWrite (pHPlusPin, LOW);
           }
-         
+
           if (pH <= HysterisPlus && pH > Setpoint && pmem == 2) {
             digitalWrite (pHMinPin, HIGH);
             delay(300);                          // LED
-            digitalWrite (pHMinPin, LOW);        // LED            
+            digitalWrite (pHMinPin, LOW);        // LED
             digitalWrite (pHPlusPin, LOW);
           }
 
@@ -223,7 +223,7 @@
           Serial.print("Setpoint = ");
           Serial.println(Setpoint);
           Serial.print("Hysteris = ");
-          Serial.println(SetHysteris);       
+          Serial.println(SetHysteris);
           Serial.print("pH = ");
           Serial.println(pH);
 
@@ -231,10 +231,10 @@
           Serial.print("Liquid Temperature: ");
           Serial.print(liqTemperature);
           Serial.println(" °C");
-                 
+
         }
-                 
-         
+
+
 
 
 
@@ -245,11 +245,11 @@
           lightInputVoltage = 5.0 * ((double)lightADCReading / 1024.0);
           // Calculating the resistance of the photoresistor in the voltage divider
           lightResistance = (10.0 * 5.0) / lightInputVoltage - 10.0;
-          // Calculating the intensity of light in lux       
+          // Calculating the intensity of light in lux
           currentLightInLux = 255.84 * pow(lightResistance, -10/9);
           //myGLCD.printNumI(currentLightInLux, 91, 162, 4);
           Serial.print("Light level = ");
-          Serial.println(currentLightInLux);        
+          Serial.println(currentLightInLux);
           }
         }
 
@@ -261,7 +261,7 @@
           if (Setpoint >= 9.00) {
             Setpoint = 9.00;
           }
-          
+
         }
 
         void phDecreaseSetpoint() {
@@ -269,7 +269,7 @@
           if (Setpoint <= 3.00) {
             Setpoint = 3.00;
           }
-          
+
         }
 
         void phIncreaseHysteris() {
@@ -277,7 +277,7 @@
           if (SetHysteris >= 9.00) {
             SetHysteris = 9.00;
           }
-          
+
         }
 
         void phDecreaseHysteris() {
@@ -285,7 +285,7 @@
           if (SetHysteris <= 0.01) {
             SetHysteris = 0.01;
           }
-          
+
         }
 
         void FanIncreaseTemp() {
@@ -293,7 +293,7 @@
           if (FanTemp >= 50) {
             FanTemp = 50;
           }
-         
+
         }
 
 
@@ -302,7 +302,7 @@
           if (FanTemp <= 0) {
             FanTemp = 0;
           }
-          
+
         }
 
 
@@ -311,7 +311,7 @@
           if (FanHumid >= 100) {
             FanHumid = 100;
           }
-     
+
         }
 
 
@@ -320,14 +320,14 @@
           if (FanHumid <= 0) {
             FanHumid = 0;
           }
-        
+
         }
 
 
         void FanControl() {                                            // control the fan based on DHT sensor T and Humididity
-          if ((h >= FanHumid) && (t >= FanTemp)) {   // if himidity is too high and temp us too high, turn fan on - 
+          if ((h >= FanHumid) && (t >= FanTemp)) {   // if himidity is too high and temp us too high, turn fan on -
             //digitalWrite(ventilatorPin, HIGH);    // actual solenoid control
-            digitalWrite(LED_FAN_PIN, HIGH);    // LED representation 
+            digitalWrite(LED_FAN_PIN, HIGH);    // LED representation
 
           }
           else {
@@ -340,11 +340,11 @@
         void TankProgControl () {
           if (getLiqLevel() < tankLowSetPoint) {
               //digitalWrite(solenoidPin, HIGH);  //open solenoid valve
-            digitalWrite(LED_SOLENOID_PIN, HIGH);    // LED representation 
+            digitalWrite(LED_SOLENOID_PIN, HIGH);    // LED representation
           }
           else {
             //digitalWrite(solenoidPin, LOW);     //close solenoid valve
-            digitalWrite(LED_SOLENOID_PIN, LOW);    // LED representation             
+            digitalWrite(LED_SOLENOID_PIN, LOW);    // LED representation
           }
         }
 
@@ -375,16 +375,16 @@
           ds.write(0x44,1); // start conversion, with parasite power on at the end
 
           byte present = ds.reset();
-          ds.select(addr);    
+          ds.select(addr);
           ds.write(0xBE); // Read Scratchpad
 
-          
+
           for (int i = 0; i < 9; i++) { // we need 9 bytes
             data[i] = ds.read();
           }
-          
+
           ds.reset_search();
-          
+
           byte MSB = data[1];
           byte LSB = data[0];
 
@@ -397,9 +397,9 @@
             digitalWrite (LED_LIQ_PIN, LOW);
           }
           return TemperatureSum;
-          
+
         }
-        
+
         void ManualRefilProg()                                        // adds liquid to tank from LCD command
         {
           digitalWrite(solenoidPin, HIGH);
@@ -407,10 +407,10 @@
         }
 
 
-        void error(char *str)                                        // error function assocaited with SD card 
+        void error(char *str)                                        // error function assocaited with SD card
         {
           Serial.print("error: ");
-          Serial.println(str); 
+          Serial.println(str);
 
           // red LED indicates error
           digitalWrite(redLEDpin, HIGH);
@@ -420,7 +420,7 @@
 
 
 
-        void SDSetup()                                                // set up SD card & RTC
+        /*void SDSetup()                                                // set up SD card & RTC
         {
 
           // initialize the SD card
@@ -443,7 +443,7 @@
               filename[7] = i%10 + '0';
               if (! SD.exists(filename)) {
                 // only open a new file if it doesn't exist
-                logfile = SD.open(filename, FILE_WRITE); 
+                logfile = SD.open(filename, FILE_WRITE);
                 break;  // leave the loop!
               }
             }
@@ -455,20 +455,20 @@
             Serial.print("Logging to: ");
             Serial.println(filename);
 
-           File dataFile = SD.open(filename, FILE_WRITE); 
+           File dataFile = SD.open(filename, FILE_WRITE);
            dataFile.print("pH,temp,humidity,light,date");
 
-        }
+        }*/
 
-     
 
-        void SDLoop()                                                // print data to SD card as a CSV
+
+        /*void SDLoop()                                                // print data to SD card as a CSV
         {
           File dataFile = SD.open(filename, FILE_WRITE);
 
           DateTime now;
           now = RTC.now();
-          
+
           if (dataFile) {
             now = RTC.now();
             dataFile.print(pH);
@@ -481,7 +481,7 @@
           //dataFile.print(", ");                       //LIQTfindMeTag
           //dataFile.print(liquidTemperatureRead()); // [] FIND A WAY TO REMOVE THE REDUNDANCY: putting this function here means that we read liq T twice each loop: once from calling liqtread in void loop, and once from SDloop. LIQTfindMeTag
             dataFile.print(", ");
-            dataFile.print(now.day(), DEC);   
+            dataFile.print(now.day(), DEC);
             dataFile.print('/');
             dataFile.print(now.month(), DEC);
             dataFile.print('/');
@@ -498,15 +498,15 @@
           else {
             Serial.println("error opening CSV file");
           }
-        }
+        }*/
 
         void timeSetup()                                            // start time
         {
-            Wire.begin();  
+            Wire.begin();
             if (!RTC.begin()) {
             logfile.println("RTC failed");
             Serial.println("RTC failed");
-            }    
+            }
         }
 
         /*
@@ -514,28 +514,28 @@
           if (Serial.available() > 0) {
             int incomingByte = Serial.read();
               switch (incomingByte) {
-                case '1':    
+                case '1':
                   phIncreaseSetpoint();                         // pH inc set point
                   break;
-                case '2':    
+                case '2':
                   phDecreaseSetpoint();                         // pH dec set point
                   break;
-                case '3':    
+                case '3':
                   phIncreaseHysteris();                         // pH inc hysteris
                   break;
-                case '4':    
+                case '4':
                   phDecreaseHysteris();                         // pH dec hysteris
                   break;
-                case '5':    
+                case '5':
                   FanIncreaseTemp();                            // fan inc temp
                   break;
-                case '6':    
+                case '6':
                   FanDecreaseTemp();                            // fan dec temp
                   break;
-                case '7':    
+                case '7':
                   FanIncreaseHumid();                           // fan inc humid
                   break;
-                case '8':    
+                case '8':
                   FanDecreaseHumid();                           // fan dec humid
                   break;
                 case '9':
@@ -552,7 +552,7 @@
           byte index = 0;
           char inData[10];
           char inChar;
-          
+
           if (Serial.available() > 0) {
             if(index < 9) { // One less than the size of the array
               inChar = toUpperCase(Serial.read()); // Read a character
@@ -589,13 +589,14 @@
           timeSetup();              // start wire and RTC ... not sure what this means specifically, but it gets the clock tickin'
           //SDSetup();                // setup SD card, report if card is missing
         }
-         
+
          void loop() {
            logicLoop();             // change control variables based on system state, serial print process variables
            fotoLoop();              // calculate and serial print light level
            FanControl();            // control fan from T and Humid
            TankProgControl();       // [] MUST REWRITE fill tank if below float level
            //SDLoop();                // log {pH, T, Humid, light, date, time} to SD card   [] ADD LIQUID TEMPERATURE
-           followSerialCommand();   // respond to serial input 
+           followSerialCommand();   // respond to serial input
+           Serial.println();
            delay(1000);
          }
