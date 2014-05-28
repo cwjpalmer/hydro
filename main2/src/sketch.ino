d90f03b0e6dc94226bd1f83cf49acd13804e5c3b
cwjpalmer <cwjpalmer@gmail.com>
2014-05-28 18:10:24 +0200

    rewrote level control logic & level sensing function to run on float switches, set of 3 floats, see comments for details; various formatting & variable nomenclature updates for readability & logical semantics; code compiles & runs, but SD code commented out, therefore usable if SD code is commented back in

--- main2/src/sketch.ino
+++ main2/src/sketch.ino
@@ -9,46 +9,50 @@
   */
 
   // ************************ Definitions **************************
-  // Libraries 
+  // Libraries
   #include <SD.h>                            //SD card library
   #include <Wire.h>                          //One Wire library
   #include "RTClib.h"                        //Real Time Clock library
-  #include <EEPROMex.h>                      //Extended Eeprom library
+  #include <EEPROMex.h>                      //Extended Eeprom library                          // not in use
   #include <OneWire.h>                       //OneWire library, for liquid temperature sensor
-  #include "DHT.h"                           //DHT library for DHT22 sensor 
-  #include "SPI.h"
+  #include "DHT.h"                           //DHT library for DHT22 sensor
+  #include "SPI.h"                                                                              // not in use
 
 
   // Pin Definitions
-  #define DHTPIN 34                          //pin for DHT22                     
-  #define redLEDpin 2                        //LEDs on SD card
-  #define greenLEDpin 3                      //LEDs on SD Card 
-  #define DS18S20_Pin 28                      //DS18S20 Signal pin on digital 2
-  #define pHPin A7                           //pin for pH probe          
-  #define pHPlusPin  45                        //pin for Base pump (relay)         
-  #define pHMinPin  46                         //pin for Acide pump (relay)        
-  #define lightSensor  A8                      //pin for Photoresistor             
-  #define solenoidPin  49                      //digital pin
-  #define LED_SOLENOID_PIN 38                //LED
-  #define LED_LIQ_PIN 40                     //LED
-  #define liqLevelRefResistor 2250           //liquid level[] it's 2250 ohms +/- 10%, so we should check it with a multimeter and put the correct value here    
-  #define liqLevelSensorPin A10              //liquid level
+  #define DHTPIN             34                //pin for DHT22
+  #define redLEDpin          2                 //LEDs on SD card
+  #define greenLEDpin        3                 //LEDs on SD Card
+  #define DS18S20_Pin        28                //DS18S20 Signal pin on digital 2
+  #define pHPin              A7                //pin for pH probe
+  #define pHPlusPin          45                //pin for Base pump (relay)
+  #define pHMinPin           46                //pin for Acide pump (relay)
+  #define lightSensor        A8                //pin for Photoresistor
+  #define solenoidPin        49                //digital pin
+  #define LED_SOLENOID_PIN   38                //LED
+  #define LED_LIQ_PIN        40                //LED
+  /*
+  #define liqLevelRefResistor 2250             //liquid level[] it's 2250 ohms +/- 10%, so we should check it with a multimeter and put the correct value here
+  #define liqLevelSensorPin A10                //liquid level
+  */
+  #define LowestFloatPin     32
+  #define MiddleFloatPin     33
+  #define HighestFloatPin    34
 
-    
   //  Setpoints
-  int tankLowSetPoint = 850;             // variable to store lower limit of tank level   // as a %
-  int tankHighSetPoint = 80;             // variable to store upper limit of tank level   // as a %  
+  int tankLowSetPoint = 60;             // variable to store lower limit of tank level   // as a %
+  int tankHighSetPoint = 80;             // variable to store upper limit of tank level   // as a %
   float Setpoint=4.8;                    //holds value for Setpoint
 
 
   //  Custom Definitions & Variables
-  #define DHTTYPE DHT22                      //DHT 22  (AM2302)  
+  #define DHTTYPE DHT22                      //DHT 22  (AM2302)
   OneWire ds(DS18S20_Pin);  //Temperature chip i/o
-  DHT dht(DHTPIN, DHTTYPE);
-  float h;                                   //humidity 
+  DHT dht(DHTPIN, DHTTYPE);                  // create a DHT type object called dht
+  float h;                                   //humidity
   float t;                                   //temperature
-  char filename[] = "LOGGER00.CSV";          //filename for CSV file    
-  File logfile;                               //indicate CSV file exists 
+  char filename[] = "LOGGER00.CSV";          //filename for CSV file
+  File logfile;                               //indicate CSV file exists
   RTC_DS1307 RTC;                            //Define RTC module
 
 
@@ -58,10 +62,10 @@
   int tankProgState = 0;
   int manualRefilState = 0;
   float pH;                          //generates the value of pH
-  int pmem = 0;                      //check which page you're on
-  float HysterisMin;                 
+  int pmem = 0;                      //check which page you're on    // I think we should get rid of this variable... it doesn't seem to be doing anything. -CP
+  float HysterisMin;
   float HysterisPlus;
-  float SetHysteris;
+  float SetHysteris = 0.1;
   int lightADCReading;
   double currentLightInLux;
   double lightInputVoltage;
@@ -69,33 +73,35 @@
   int EepromSetpoint = 10;      //location of Setpoint in Eeprom
   int EepromSetHysteris = 20;   //location of SetHysteris in Eeprom
   float liqTemperatureOutput;
-  byte bGlobalErr;    //for passing error code back.
+//  byte bGlobalErr;              //for passing error code back.      // not in use (only appeaers here) => commented out   -CP
+  bool fillingNow = false;
+  bool overFilledError = false;
 
-  
-  //  Looped Functions         
+
+  //  Looped Functions
 
   // ********************* Setup Functions ***********************
 
-  //  Non-Volatile Arduino Memory 
-  void EepromRead() {  
+  //  Non-Volatile Arduino Memory
+  void EepromRead() {
       //Setpoint = EEPROM.readFloat(EepromSetpoint);
       //SetHysteris = EEPROM.readFloat(EepromSetHysteris);
     }
 
   //  Configuration of pins
-  void logicSetup() {        
+  void logicSetup() {
 
     pinMode(pHPlusPin, OUTPUT);
     pinMode(pHMinPin, OUTPUT);
     pinMode(solenoidPin, OUTPUT);
     pinMode(LED_SOLENOID_PIN, OUTPUT);
     pinMode (LED_LIQ_PIN, OUTPUT);
-    pinMode(10, OUTPUT);
+    pinMode(10, OUTPUT);                    // I think this is the SD pin -CP
     pinMode(redLEDpin, OUTPUT);
     pinMode(greenLEDpin, OUTPUT);
-   
+
     pmem==0;
-     
+
     delay(300);
     Serial.begin(9600);
     Serial.println();
@@ -105,6 +111,7 @@
 
   }
 
+/*                      // RMed for testing without SD card reader
   void SDSetup() {
     // initialize the SD card
     Serial.print("Initializing SD card...");
@@ -120,7 +127,7 @@
         filename[7] = i%10 + '0';
         if (! SD.exists(filename)) {
           // only open a new file if it doesn't exist
-          logfile = SD.open(filename, FILE_WRITE); 
+          logfile = SD.open(filename, FILE_WRITE);
           break;  // leave the loop!
         }
       }
@@ -132,27 +139,61 @@
       Serial.print("Logging to: ");
       Serial.println(filename);
 
-     File dataFile = SD.open(filename, FILE_WRITE); 
+     File dataFile = SD.open(filename, FILE_WRITE);
      dataFile.print("pH,temp,humidity,light,date");
   }
+*/
 
-
+/*
+  ***** LIQUID LEVEL FUNCTION BLOCK *****
+      *  contains setup & loop functions
+      *  uses float switches
+      *  loop function checks if liquid level is too low
+      *  returns bool; true = too low, false = not too low = acceptable level
+*/
+  void TankShouldFillSetup() {
+    pinMode(LowestFloatPin,INPUT);
+    pinMode(MiddleFloatPin,INPUT);
+    pinMode(HighestFloatPin,INPUT);
+  }
   //  Liquid Level Sensor Function
-  int getLiqLevel() {
-    float reading;
+  int TankShouldFillLoop() {  // return bool // formerly TankShouldFillLoop
+    bool result;
 
-    reading = analogRead(liqLevelSensorPin);
-    // convert the value to resistance
-    reading = (1023 / reading)  - 1;
-    reading = liqLevelRefResistor / reading;
-    Serial.print("Sensor resistance "); 
-    Serial.println(reading);
+    bool lowestFloatSubmerged = false;
+    bool middleFloatSubmerged = false;
+    bool highestFloatSubmerged = false;
 
-    return reading;
+    lowestFloatSubmerged = digitalRead(LowestFloatPin);
+    middleFloatSubmerged = digitalRead(MiddleFloatPin);
+    highestFloatSubmerged = digitalRead(HighestFloatPin);
+
+    if (! lowestFloatSubmerged) {    // this else-if statement is overkill, but I included redundant statements for clarity of behaviour -CP
+      // fill
+      result = true;
+    } else if (fillingNow && ! middleFloatSubmerged) {
+      // fill
+      result = true;
+    } else if (middleFloatSubmerged) {
+      // stop filling
+      result = false;
+    } else {
+      // nothing
+    }
+
+    if (highestFloatSubmerged) {
+      overFilledError = true;
+      result = false;
+    } else {
+      overFilledError = false;
+    }
+
+    return result;
   }
 
 
-  void logicLoop() { 
+  void logicLoop() {
+
 
   h = dht.readHumidity();
   t = dht.readTemperature();
@@ -161,21 +202,24 @@
   if (isnan(t) || isnan(h)) {
     Serial.println("Failed to read from DHT");
   } else {
-    Serial.print("Humidity: "); 
+    Serial.print("Humidity: ");
     Serial.print(h);
     Serial.print(" % RH\t");
-    Serial.print("Temperature: "); 
+    Serial.print("Temperature: ");
     Serial.print(t);
-    Serial.println(" °C");        
-    }      
-                     
-    float sensorValue = 0;
-    sensorValue = analogRead(pHPin);
-    pH = (0.0178 * sensorValue - 1.889);
+    Serial.println(" °C");
+    }
+    /*                        [ ] CP CHANGE BACK
+    float pHSensorValue = 0;
+    pHSensorValue = analogRead(pHPin);
+    pH = (0.0178 * pHSensorValue - 1.889);
+    */
+
+    float pH = 5.4;  // CP KILL THIS - its BS BS BS
 
     HysterisMin = (Setpoint - SetHysteris);
     HysterisPlus = (Setpoint + SetHysteris);
-     
+
     // - SERIES OF IF STATEMENTS TO CHANGE CONTROL VARIABLES BASED ON SYSTEM STATE -
     if (pH == Setpoint) {
       pmem == 0, // why is this a logical operator?
@@ -187,7 +231,7 @@
       digitalWrite (pHMinPin, LOW);
       digitalWrite (pHPlusPin, LOW);
     }
-   
+
     if (pH < HysterisMin && pmem == 0) {
       pmem == 1,
       digitalWrite (pHPlusPin, HIGH);
@@ -195,33 +239,33 @@
       digitalWrite (pHPlusPin, LOW);       // LED
       digitalWrite (pHMinPin, LOW);
     }
-   
+
     if (pH >= HysterisMin && pH < Setpoint && pmem == 1) {
       digitalWrite (pHPlusPin, HIGH);
       delay(300);                          // LED
-      digitalWrite (pHPlusPin, LOW);       // LED            
+      digitalWrite (pHPlusPin, LOW);       // LED
       digitalWrite (pHMinPin, LOW);
     }
-   
+
     if (pH > HysterisPlus && pmem == 0) {
       pmem ==2,
       digitalWrite (pHMinPin, HIGH);
       delay(300);                          // LED
-      digitalWrite (pHMinPin, LOW);        // LED            
-      digitalWrite (pHPlusPin, LOW);
-    }
-   
-    if (pH <= HysterisPlus && pH > Setpoint && pmem == 2) {
-      digitalWrite (pHMinPin, HIGH);
-      delay(300);                          // LED
-      digitalWrite (pHMinPin, LOW);        // LED            
+      digitalWrite (pHMinPin, LOW);        // LED
       digitalWrite (pHPlusPin, LOW);
     }
 
-    Serial.print("Setpoint = ");
+    if (pH <= HysterisPlus && pH > Setpoint && pmem == 2) {
+      digitalWrite (pHMinPin, HIGH);
+      delay(300);                          // LED
+      digitalWrite (pHMinPin, LOW);        // LED
+      digitalWrite (pHPlusPin, LOW);
+    }
+
+    Serial.print("pH Setpoint = ");
     Serial.println(Setpoint);
-    Serial.print("Hysteris = ");
-    Serial.println(SetHysteris);       
+    Serial.print("pH Hysteris = ");
+    Serial.println(SetHysteris);
     Serial.print("pH = ");
     Serial.println(pH);
 
@@ -229,10 +273,10 @@
     Serial.print("Liquid Temperature: ");
     Serial.print(liqTemperature);
     Serial.println(" °C");
-           
+
   }
-               
-       
+
+
 
 
   //  Determine amount of light, in lux
@@ -243,10 +287,10 @@
       lightInputVoltage = 5.0 * ((double)lightADCReading / 1024.0);
       // Calculating the resistance of the photoresistor in the voltage divider
       lightResistance = (10.0 * 5.0) / lightInputVoltage - 10.0;
-      // Calculating the intensity of light in lux       
+      // Calculating the intensity of light in lux
       currentLightInLux = 255.84 * pow(lightResistance, -10/9);
       Serial.print("Light level = ");
-      Serial.println(currentLightInLux);        
+      Serial.println(currentLightInLux);
     }
   }
 
@@ -279,14 +323,23 @@
   }
 
 
-  void TankProgControl () {
-    if (getLiqLevel() < tankLowSetPoint) {
+  void TankLevelControlLoop () {
+    if (TankShouldFillLoop()) {
         //digitalWrite(solenoidPin, HIGH);  //open solenoid valve
-      digitalWrite(LED_SOLENOID_PIN, LOW);    // LED representation 
-    }
-    else {
+      digitalWrite(LED_SOLENOID_PIN, HIGH);    // LED representation
+      fillingNow = true;
+    } else {
       //digitalWrite(solenoidPin, LOW);     //close solenoid valve
-      digitalWrite(LED_SOLENOID_PIN, HIGH);    // LED representation             
+      digitalWrite(LED_SOLENOID_PIN, LOW);    // LED representation
+      fillingNow = false;
+    }
+
+    if (overFilledError) {
+      Serial.println("Nutrient tank is overfilled and may overflow. Check tank & related hardware, esp. fill solenoid & feeding pump.");
+    } else if (fillingNow) {
+      Serial.println("Nutrient tank is filling.");
+    } else {
+      Serial.println("Nutrient tank level is within acceptable range.");
     }
   }
 
@@ -318,16 +371,16 @@
     ds.write(0x44,1); // start conversion, with parasite power on at the end
 
     byte present = ds.reset();
-    ds.select(addr);    
+    ds.select(addr);
     ds.write(0xBE); // Read Scratchpad
 
-    
+
     for (int i = 0; i < 9; i++) { // we need 9 bytes
       data[i] = ds.read();
     }
-    
+
     ds.reset_search();
-    
+
     byte MSB = data[1];
     byte LSB = data[0];
 
@@ -339,38 +392,38 @@
     else {
       digitalWrite (LED_LIQ_PIN, LOW);
     }
-    liqTemperatureOutput = TemperatureSum; 
+    liqTemperatureOutput = TemperatureSum;
     return TemperatureSum;
-    
-  }
-  
 
-  //  Manual Refill via Open Solenoid Valve 
+  }
+
+
+  //  Manual Refill via Open Solenoid Valve
   void ManualRefilProg() {
     digitalWrite(solenoidPin, HIGH);
   }
 
 
   //  Error Function Associated with SD Card
-  void error(char *str)                    
+  void error(char *str)
   {
     Serial.print("error: ");
-    Serial.println(str); 
+    Serial.println(str);
 
     // red LED indicates error
     digitalWrite(redLEDpin, HIGH);
     while(1);
   }
 
-   
+
   //  Print Data to SD Card
-  void SDLoop()                                               
+  void SDLoop()
   {
     File dataFile = SD.open(filename, FILE_WRITE);
 
     DateTime now;
     now = RTC.now();
-    
+
     if (dataFile) {
       now = RTC.now();
       dataFile.print(pH);
@@ -391,13 +444,13 @@
   }
 
 
-  void timeSetup()                                   
+  void timeSetup()
   {
-      Wire.begin();  
+      Wire.begin();
       if (!RTC.begin()) {
       logfile.println("RTC failed");
       Serial.println("RTC failed");
-      }    
+      }
   }
 
 
@@ -406,34 +459,35 @@
     if (Serial.available() > 0) {
       int incomingByte = Serial.read();
         switch (incomingByte) {
-          case '1':    
+          case '1':
             phIncreaseSetpoint();                         // pH inc set point
             break;
-          case '2':    
+          case '2':
             phDecreaseSetpoint();                         // pH dec set point
             break;
           default:
             Serial.println('Invalid input. Enter 1-9');
-            break;  
+            break;
         }
     }
   }
 
-
   // *********************** Main Loops **************************
   void setup() {
     EepromRead();             //  pull values for Setpoint, SetHysteris, from eeprom
-    dht.begin();              // 
+    dht.begin();              //
     logicSetup();             //  set some pinmodes and begin serial comms
     timeSetup();              //  start wire and RTC ... not sure what this means specifically, but it gets the clock tickin'
-    SDSetup();                //  setup SD card, report if card is missing
+    //SDSetup();                //  setup SD card, report if card is missing
+    TankShouldFillSetup();
   }
-   
+
    void loop() {
      logicLoop();             //  change control variables based on system state, serial print process variables
      lightLoop();             //  calculate and serial print light level
-     TankProgControl();       //  f
-     SDLoop();                //  f
-     followSerialCommand();   // respond to serial input 
-     delay(4000);         
+     TankLevelControlLoop();       //  f
+     //SDLoop();                //  f
+     followSerialCommand();   // respond to serial input
+     Serial.println();
+     delay(4000);
    }
